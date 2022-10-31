2b297612a718059c83eae2a4b863a0c80505c3f2
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-10-31 03:16:45 -0300

    :zap: Otimizando a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
 
 import { api } from './services/api';
 
@@ -54,9 +54,9 @@ export function App() {
       });
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, []);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { Icon } from './Icon';
 
 import '../styles/button.scss';
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, memo, useMemo } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
@@ -9,11 +9,21 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   selected: boolean;
 }
 
-export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
+function ButtonComponent({ iconName, title, selected, ...rest }: ButtonProps) {
+  const className = useMemo(() => {
+    return selected ? 'selected' : '';
+  }, [selected]);
+
+  const color = useMemo(() => {
+    return selected ? '#FAE800' : '#FBFBFB';
+  }, [selected]);
+
   return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+    <button type="button" className={className} {...rest}>
+      <Icon name={iconName} color={color} />
       {title}
     </button>
   );
 }
+
+export const Button = memo(ButtonComponent);
